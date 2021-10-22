9d69b1cd4d2a4920706fdf553b1714e032c19e60
banchose <wjs@fastmail.net>
2021-10-22 12:30:06 -0400

    added dot files

---
+++ disperse.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cp -v -- ./.bashrc ~/
+cp -v -- ./.tmux.conf ~/
+cp -v -- ./.inputrc  ~/
+# [[ ! -d ~/.vim ]] && mkdir ~/.vim
+# cp -v ./vim/vimrc  ~/.vim/
+# [[ ! -d ~/.config/nvim ]] && mkdir -p ~/.config/nvim
+# cp -v -- ./vim/init.vim ~/.config/nvim
+# cp -v -- ./vim/ginit.vim ~/.config/nvim
+# cp -v -- ./vim/init.vim ~/.vim/vimrc
+# cp -v -- ./.gdbinit  ~/
+# cp -v -- ./.bcrc ~/
