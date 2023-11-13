ecbe5981808e254e36dbdbb03c831040c7b3be12
banchose <a_gitmail@ownmail.net>
2023-11-12 22:59:43 -0500

    lkjl

---
+++ docker/apk-essential.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+set -euo pipefail
+
+(
+	apk add git tmux jq nmap procps-ng perl the_silver_searcher the_silver_searcher-bash-completion the_silver_searcher-doc procs-bash-completion rust 7zip 7zip-doc zip unzip tshark fd ripgrep vim nano neovim curl wget aria2 socat rsync ncurses rclone iproute2 gcc make cmake gdb curl wget aria2 rclone rsync openssh nginx socat bcc py3-pip py3-pip-doc py3-pip-bash-completion py3-setuptools
+	cd
+	mkdir temp gitdir
+	[[ -d ~/gitdir/skel ]] || git clone https://github.com/banchose/skel ~/gitdir/skel
+	[[ -d ~/.config/nvim ]] || git clone https://github.com/lazyvim/starter ~/.config/nvim
+)
