29f7ae63611af22d0c79ebe06a552e6d6be81644
banchose <a_gitmail@ownmail.net>
2024-04-25 16:35:14 -0400

    who

--- MAIN-mak-kube-node.sh
+++ MAIN-mak-kube-node.sh
@@ -5,19 +5,17 @@ set -x
 
 version=1.28.4-00
 
-# Ensure essential packages are installed
-sudo apt-get update
-sudo apt-get install -y curl software-properties-common apt-transport-https ca-certificates git jq
-
-# Download and add the GPG key for Kubernetes packages
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-# sudo install -o root -g root -m 644 ./kubernetes-archive-keyring.gpg /usr/share/keyrings/kubernetes-archive-keyring.gpg
-
 # Add Kubernetes APT repository
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
+# Download and add the GPG key for Kubernetes packages
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# sudo install -o root -g root -m 644 ./kubernetes-archive-keyring.gpg /usr/share/keyrings/kubernetes-archive-keyring.gpg
+
+
 # Update APT and install Kubernetes components
 sudo apt update
+sudo apt-get install -y curl software-properties-common apt-transport-https ca-certificates git jq
 sudo apt-get install -y kubelet="${version}" kubeadm="${version}" kubectl="${version}" containerd
 sudo apt-mark hold kubelet kubeadm kubectl
 
