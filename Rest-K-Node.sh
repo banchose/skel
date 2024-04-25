643c121653bb3522b97a861e44bde96e681ab63b
banchose <a_gitmail@ownmail.net>
2024-04-25 19:45:47 -0400

    who

---
+++ Rest-K-Node.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+set -euo pipefail
+
+grep -i ubuntu /etc/os-release || {
+	echo "Are you Ubuntu?"
+}
+exit 1
+sudo systemctl stop kubelet || echo "kubelet not running"
+sudo systemctl disable kubelet || echo "Disabling kubelet was a disaster"
+sudo rm -rf /etc/kubernetes
+sudo rm -rf /var/lib/kubelet
+sudo rm -rf /var/lib/etcd
+sudo rm -rf /etc/machine-id
+sudo dpkg-reconfigure openssh-server
+echo "===================> Change the hostname"
