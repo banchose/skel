c1d2c680080e34e474d110620df038caf57da7d4
banchose <a_gitmail@ownmail.net>
2024-04-25 21:15:26 -0400

    who

--- Rest-K-Node.sh
+++ Rest-K-Node.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
 #
+#!/usr/bin/env bash
 set -euo pipefail
 
-grep -s -i ubuntu /etc/os-release || {
+grep -i ubuntu /etc/os-release || {
 	echo "Are you Ubuntu?"
-	exit 1
 }
+exit 1
 sudo systemctl stop kubelet || echo "kubelet not running"
 sudo systemctl disable kubelet || echo "Disabling kubelet was a disaster"
 sudo rm -rf /etc/kubernetes
 sudo rm -rf /var/lib/kubelet
 sudo rm -rf /var/lib/etcd
-sudo rm -rf /etc/machine-id
+sudo rm -f /etc/machine-id /etc/ssh/ssh_host_*
 sudo dpkg-reconfigure openssh-server
+sudo systemctl restart ssh
+sudo dbus-uuidgen --ensure=/etc/machine-id
+sudo ssh-keygen -A
 echo "===================> Change the hostname"
