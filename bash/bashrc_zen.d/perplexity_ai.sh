d3830b82b1b209c782c7b7dd63ce21023f319282
banchose <a_gitmail@ownmail.net>
2024-05-03 20:22:27 -0400

    who

--- bash/bashrc_zen.d/perplexity_ai.sh
+++ bash/bashrc_zen.d/perplexity_ai.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-tell() {
-	# Ensure the first argument is provided
-	if [ -z "$1" ]; then
-		echo "Usage: $0 'Your question here'"
-		return 1
-	fi
-
-	# Replace 'abc' with your actual authorization token
-	AUTH_TOKEN=""
-	MODEL="sonar-medium-online"
-	QUESTION="$1"
-	LOG=~/temp/perplexity_ai.log
-	DATA='
+KeyFile=~/sec/perplexity_key.env
+AUTH_TOKEN=""
+MODEL="sonar-medium-online"
+QUESTION="$1"
+LOG=~/temp/perplexity_ai.log
+DATA='
   {
     "model": "'"$MODEL"'",
     "messages": [
       {
         "role": "system",
-        "content": "Favor precise and concise. Your location is the state of New York in the USA. You are in the Eastern Standard time zone. The current date and time here is: '"$(date)"'"
+        "content": "Favor precise and concise. The current date and time in your location (Albany, NY) is: '"$(date)"'"
       },
       {
         "role": "user",
@@ -27,11 +20,26 @@ tell() {
     ]
   }
   '
+
+tell() {
+	# Ensure the first argument is provided
+	if [ -z "$1" ]; then
+		echo "Usage: $0 'Your question here'"
+		return 1
+	fi
+
+	[[ -r $KeyFile ]] || {
+		echo "Can't read $KeyFile"
+		return 0
+	}
+
+	source "$KeyFile"
+
 	# Use the variable in the data payload
 	curl -s --request POST \
 		--url https://api.perplexity.ai/chat/completions \
 		--header "accept: application/json" \
-		--header "authorization: Bearer $AUTH_TOKEN" \
+		--header "authorization: Bearer $Perplexity_api_key" \
 		--header "content-type: application/json" \
 		--data "$DATA" | jq '.choices[0].message.content' | sed -e 's/\"//g' -e 's/\\n/\n/g' | fmt
 	{
