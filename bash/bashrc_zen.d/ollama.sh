021fdf6455533f3c367c9c9973287dd7d7c96144
banchose <a_gitmail@ownmail.net>
2025-01-12 03:10:28 +0000

    ollama.sh

--- bash/bashrc_zen.d/ollama.sh
+++ bash/bashrc_zen.d/ollama.sh
@@ -13,3 +13,25 @@ ol () {
 	echo " ollama run ${model}"
 	ollama run "${model// */}"
 }
+
+
+echo "**** BASH COMPLETION DEFINED ****"
+echo "**** ollama bash completion ****"
+
+_complete_ollama() {
+    local cur prev words cword
+    _init_completion -n : || return
+
+    if [[ ${cword} -eq 1 ]]; then
+        COMPREPLY=($(compgen -W "serve create show run push pull list ps cp rm help" -- "${cur}"))
+    elif [[ ${cword} -eq 2 ]]; then
+        case "${prev}" in
+            (run|show|cp|rm|push|list)
+                WORDLIST=$((ollama list 2>/dev/null || echo "") | tail -n +2 | cut -d "	" -f 1)
+                COMPREPLY=($(compgen -W "${WORDLIST}" -- "${cur}"))
+                __ltrim_colon_completions "$cur"
+                ;;
+        esac
+    fi
+}
+complete -F _complete_ollama ollama
