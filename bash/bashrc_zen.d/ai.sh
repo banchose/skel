03ff8969b6cb1bdbd093292920995b4e3211d43a
banchose <a_gitmail@ownmail.net>
2025-01-20 14:09:33 -0500

    qa and openrouter ai.sh

--- bash/bashrc_zen.d/ai.sh
+++ bash/bashrc_zen.d/ai.sh
@@ -1,31 +1,40 @@
-# Fill in the key ENVs
 source ~/.bashrc_zen.d/api_keys_envs.sh
-# EndPoint="https://api.perplexity.ai/chat/completions"
-EndPoint=https://openrouter.ai/api/v1/chat/completions
-
+# 0. Import Api keys
+# 1. EndPoint
+# 2. Model
+# 3. Max tokens
+# 4. Token key
 #
+# Endpoint
+#
+PPTX_EndPoint="https://api.perplexity.ai/chat/completions"
+OR_ENDPOINT="https://openrouter.ai/api/v1/chat/completions"
+
 # Models
+#
 ## Perplexity
 #
 # Model="llama-3.1-sonar-small-128k-online" # 8B
 # Model="llama-3.1-sonar-large-128k-online" # 70B
-# Model="llama-3.1-sonar-huge-128k-online" # 405B
+PPTX_Model="llama-3.1-sonar-huge-128k-online" # 405B
+OR_MODEL="mistralai/ministral-8b"
 #
 ## OpenRouter
 #
 # Model="mistralai/mistral-nemo"
-Model="deepseek/deepseek-chat"
+# Model="deepseek/deepseek-chat"
+# Model="anthropic/claude-3.5-sonnet"
 #
 # Tokens
 #
 Max_Input_Tokens=2048
 Max_Tokens=500
-
-# Perplexity_AI_EndPoint="https://api.perplexity.ai/chat/completions"
-
-# API_KEY="${PERPLEXITY_API_KEY}"
-API_KEY="${OPENROUTER_API_KEY}"
-echo "${OPENROUTER_API_KEY}"
+#
+# API Key
+#
+API_KEY="${PERPLEXITY_API_KEY}"
+# API_KEY="${OPENROUTER_API_KEY}"
+# echo "${OPENROUTER_API_KEY}"
 echo "$API_KEY"
 # System prompt
 #
@@ -48,7 +57,11 @@ sanitize_input() {
 #
 # echo "$(sanitize_input "${input_stdin}")"
 
-qa() {
+qx() {
+  local content
+  local API_KEY=
+  local EndPoint=
+
   # Check if API key is set
   if [[ -z "$API_KEY" ]]; then
     echo "Error: API_KEY is not defined. Please set it and try again." >&2
@@ -92,12 +105,132 @@ qa() {
           "content": "'"${Sanitized_Input}"'"
         }
       ]
-    }' | tee --append ~/temp/answers.json | jq -r '
+    }' | tee --append ~/temp/answers.json | jq -r --arg api_key "$API_KEY" '
           "Prompt tokens: \(.usage.prompt_tokens)\n" +
           "Total tokens: \(.usage.total_tokens)\n" +
           "Completion tokens: \(.usage.completion_tokens)\n" +
           "Finish reason: \(.choices[0].finish_reason)\n" +
-          "Model: \(.model)\n\n" +
+          "Model: \(.model)\n" +
+          "Abbreviated key: \($api_key | split("-")[0])\n\n" +
+          .choices[0].message.content
+        '
+
+}
+
+qp() {
+  local content
+  local API_KEY="${PERPLEXITY_API_KEY}"
+  local EndPoint="${PPTX_EndPoint}"
+  # Check if API key is set
+  if [[ -z "$API_KEY" ]]; then
+    echo "Error: API_KEY is not defined. Please set it and try again." >&2
+    return 1
+  fi
+
+  # Check if input is provided either as a parameter or from stdin
+  if [[ -n "$1" ]]; then
+    content="$1"
+  elif ! tty -s && read -r content; then
+    : # Input from stdin
+  else
+    echo "Error: No input provided. Please provide input as a parameter or via stdin." >&2
+    return 1
+  fi
+
+  # Sanitize the input
+  local Sanitized_Input
+  Sanitized_Input=$(sanitize_input "$content")
+
+  # API call with sanitized content
+  curl -s --location "${EndPoint}" \
+    --header 'Accept: Application/json' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer ${API_KEY}" \
+    --data '{
+      "model": "'"${Model}"'",
+      "stream": false,
+      "return_related_questions": false,
+      "return_images": false,
+      "search_recency_filter": "month",
+      "max_tokens": '"${Max_Tokens}"',
+      "messages": [
+        {
+          "role": "system",
+          "content": "'"${System_Prompt}"'"
+        },
+        {
+          "role": "user",
+          "content": "'"${Sanitized_Input}"'"
+        }
+      ]
+    }' | tee --append ~/temp/answers.json | jq -r --arg api_key "$API_KEY" '
+          "Prompt tokens: \(.usage.prompt_tokens)\n" +
+          "Total tokens: \(.usage.total_tokens)\n" +
+          "Completion tokens: \(.usage.completion_tokens)\n" +
+          "Finish reason: \(.choices[0].finish_reason)\n" +
+          "Model: \(.model)\n" +
+          "Abbreviated key: \($api_key | split("-")[0])\n\n" +
+          .choices[0].message.content
+        '
+
+}
+
+qa() {
+
+  local content
+  local API_KEY="${OPENROUTER_API_KEY}"
+  local EndPoint="${OR_ENDPOINT}"
+  local Model="${OR_MODEL}"
+
+  # Check if API key is set
+  if [[ -z "$API_KEY" ]]; then
+    echo "Error: API_KEY is not defined. Please set it and try again." >&2
+    return 1
+  fi
+
+  # Check if input is provided either as a parameter or from stdin
+  if [[ -n "$1" ]]; then
+    content="$1"
+  elif ! tty -s && read -r content; then
+    : # Input from stdin
+  else
+    echo "Error: No input provided. Please provide input as a parameter or via stdin." >&2
+    return 1
+  fi
+
+  # Sanitize the input
+  local Sanitized_Input
+  Sanitized_Input=$(sanitize_input "$content")
+
+  # API call with sanitized content
+  curl -s --location "${EndPoint}" \
+    --header 'Accept: Application/json' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer ${API_KEY}" \
+    --data '{
+      "model": "'"${Model}"'",
+      "stream": false,
+      "return_related_questions": false,
+      "return_images": false,
+      "search_recency_filter": "month",
+      "max_tokens": '"${Max_Tokens}"',
+      "messages": [
+        {
+          "role": "system",
+          "content": "'"${System_Prompt}"'"
+        },
+        {
+          "role": "user",
+          "content": "'"${Sanitized_Input}"'"
+        }
+      ]
+    }' | tee --append ~/temp/answers.json | jq -r --arg api_key "$API_KEY" '
+          "Prompt tokens: \(.usage.prompt_tokens)\n" +
+          "Total tokens: \(.usage.total_tokens)\n" +
+          "Completion tokens: \(.usage.completion_tokens)\n" +
+          "Finish reason: \(.choices[0].finish_reason)\n" +
+          "Model: \(.model)\n" +
+          "Abbreviated key: \($api_key | split("-")[0])\n\n" +
           .choices[0].message.content
         '
 
