6b68db0d24d19a828f92d438d1f6ad47f0278078
banchose <a_gitmail@ownmail.net>
2025-02-07 23:22:34 -0500

    who

--- nvim/plugins/oil.lua
+++ nvim/plugins/oil.lua
@@ -1,13 +1,26 @@
 return {
   "stevearc/oil.nvim",
   config = function()
+    local detail = false
+
     require("oil").setup({
       default_file_explorer = true,
-      columns = { "icon" }, -- Customize columns (e.g., "permissions", "size", etc.)
+      columns = { "icon" }, -- Default column setup
       keymaps = {
         ["<BS>"] = "actions.parent",
         ["<CR>"] = "actions.select",
         ["_"] = "actions.open_cwd",
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
       },
       view_options = {
         show_hidden = true, -- Show hidden files
