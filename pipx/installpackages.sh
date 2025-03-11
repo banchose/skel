a3356adba7a7751c59084d53f9d1e53e849b5905
banchose <a_gitmail@ownmail.net>
2025-03-11 14:34:25 -0400

    pipx install packages script

---
+++ pipx/installpackages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+pipx install checkov
+pipx install csvkit
+pipx install twisted
+pipx install httpie
+pipx install pgcli
+pipx install mycli
+pipx install litecli
+pipx install ruff
+pipx install black
+pipx install flake8
+pipx install pylint
