c9f5aba5de44d28b402da005218e099fbf6c94bc
banchose <a_gitmail@ownmail.net>
2025-03-19 19:08:50 -0400

    ongoing

--- pipx/installpackages.sh
+++ pipx/installpackages.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# sudo apt update && sudo apt install python3-venv python3-pip
+
 # pip install --user pipx
+# command -v pipx >/dev/null &>/dev/null || pip install --user pipx
 # pipx ensurepath
+# pipx completions
+# echo 'eval "$(register-python-argcomplete pipx)"' >> ~/.bashrc
+# source ~/.bashrc
+# pipx environment
 # PyPi
 # pipx completions
 pipx install checkov --include-deps
 # pipx install ipython --include-deps
 pipx install csvkit --include-deps
 pipx install twisted --include-deps
+pipx install ipython --include-deps
+# pipx install ansible --include deps
 pipx install httpie --include-deps
 pipx install pgcli --include-deps
 pipx install mycli --include-deps
@@ -26,3 +35,10 @@ pipx install glances --include-deps
 pipx install jupyter --include-deps
 pipx install cookiecutter --include-deps
 pipx install visidata --include-deps
+pipx install esptool --include-deps
+pipx install shell-functools --include-deps
+pipx install isort --include-deps
+pipx install flask --include-deps
+pipx install django --include-deps
+pipx install pandas --include-deps
+pipx install speedtest --include-deps
