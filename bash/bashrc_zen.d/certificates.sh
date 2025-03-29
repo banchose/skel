ada7d8f2b47b0c8d85da0e4509184cc5403f9b14
banchose <a_gitmail@ownmail.net>
2025-03-29 12:49:30 -0400

    get certificates

--- bash/bashrc_zen.d/certificates.sh
+++ bash/bashrc_zen.d/certificates.sh
@@ -1,3 +1,22 @@
+# When pasting certificates into a text box (such as in a control panel or configuration interface):
+#
+# 1. Place your end-entity (leaf) certificate at the top/first
+# 2. Then place the intermediate certificate(s) below it
+#
+# Each certificate will have its own BEGIN and END delimiters:
+#
+# ```
+# -----BEGIN CERTIFICATE-----
+# (your end-entity certificate content)
+# -----END CERTIFICATE-----
+# -----BEGIN CERTIFICATE-----
+# (intermediate certificate content)
+# -----END CERTIFICATE-----
+# ```
+#
+# The system reading this format will understand that these are separate certificates in the chain. The order matters - your certificate must come first, followed by the intermediate certificate(s).
+#
+
 getcert() {
 
   local AHOST
