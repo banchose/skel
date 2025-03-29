1ef347c8d225f7523bc2eede2d678f90d7394ac5
banchose <a_gitmail@ownmail.net>
2025-03-29 12:45:03 -0400

    get certificates

--- bash/bashrc_zen.d/certificates.sh
+++ bash/bashrc_zen.d/certificates.sh
@@ -1,5 +1,8 @@
 getcert() {
 
-  openssl s_client -connect star.xaax.dev:443 -showcerts </dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | openssl x509 -text -noout
+  local AHOST
+  AHOST="${1}"
+
+  openssl s_client -connect "${AHOST}":443 -showcerts </dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | openssl x509 -text -noout
 
 }
