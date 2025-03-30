9eb8fcb5acebc2754c43fbc52a0e74a1c347aa46
banchose <a_gitmail@ownmail.net>
2025-03-30 16:34:59 +0000

    who

---
+++ docker/compose/MAKE-APP-DIRS.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+mkdir -p -- ./config
+mkdir -p -- ./dashy
+mkdir -p -- ./data/watchYourLAN
+mkdir -p -- ./etc/{certs,traefik}
+mkdir -p -- ./homer
+mkdir -p -- ./logs
+mkdir -p -- ./meilisearch_data
+mkdir -p -- ./mongodb_data
+mkdir -p -- ./nginx-content
+mkdir -p -- ./privatebin-data
+mkdir -p -- ./uploads
