2c57f7ccb2c39678b3ff39277349b5e7f0074e63
banchose <a_gitmail@ownmail.net>
2025-03-30 12:49:32 -0400

    who

--- bash/bashrc_zen.d/certificates.sh
+++ bash/bashrc_zen.d/certificates.sh
@@ -25,3 +25,12 @@ getcert() {
   openssl s_client -connect "${AHOST}":443 -showcerts </dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | openssl x509 -text -noout
 
 }
+
+getcn() {
+
+  local AHOST
+  AHOST="${1}"
+
+  openssl s_client -connect "${AHOST}":443 -showcerts </dev/null | sed -n '/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p' | openssl x509 -text -noout | grep 'CN:'
+
+}
