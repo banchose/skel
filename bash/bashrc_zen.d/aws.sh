cc2d6508c7d56cfde234721c68db6987518a006e
banchose <a_gitmail@ownmail.net>
2025-04-07 16:53:18 -0400

    who

--- bash/bashrc_zen.d/aws.sh
+++ bash/bashrc_zen.d/aws.sh
@@ -124,12 +124,13 @@ set_aws_envs() {
   set_stack_outputs HRI-BIGAWSDNS us-east-1 net
   set_stack_outputs HRI-BIGDEV us-east-1 dev
   set_stack_outputs HRI-BIGTEST us-east-1 test
-  set_stack_outputs HRI-BIGDATA us-east-1 production # Mind the profile
-  set_stack_outputs HRI-BIGEKSALB us-east-1 test     # Mind the profile
-  set_stack_outputs HRI-VPC-ONLY-EKS us-east-1 test  # Mind the profile
-  set_stack_outputs EKS-VPC-ONLY2 us-east-1 test     # Mind the profile
-  set_stack_outputs HRI-BIGEKSALB us-east-1 test     # Mind the profile
-  set_stack_outputs HRI-BIGEKSALB2 us-east-1 test    # Mind the profile
+  set_stack_outputs HRI-BIGDATA us-east-1 production     # Mind the profile
+  set_stack_outputs HRI-BIGEKSALB us-east-1 test         # Mind the profile
+  set_stack_outputs HRI-VPC-ONLY-EKS us-east-1 test      # Mind the profile
+  set_stack_outputs EKS-VPC-ONLY2 us-east-1 test         # Mind the profile
+  set_stack_outputs HRI-BIGEKSALB us-east-1 test         # Mind the profile
+  set_stack_outputs HRI-BIGEKSALB2 us-east-1 test        # Mind the profile
+  set_stack_outputs HRI-EKSVPC-PROD us-east-1 production # Mind the profile
 
 }
 
