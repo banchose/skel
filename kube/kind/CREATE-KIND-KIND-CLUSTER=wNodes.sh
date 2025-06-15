b7ebbd3253028f83f0260b1e0108b5c0ea620be6
banchose <a_gitmail@ownmail.net>
2025-06-15 17:24:31 -0400

    shoe

---
+++ kube/kind/CREATE-KIND-KIND-CLUSTER=wNodes.sh
@@ -0,0 +1,26 @@
+cat ./create-cluster-with-settings.sh
+#!/usr/bin/env bash
+
+set -ueo pipefail
+
+cat <<EOF | ~/go/bin/kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+- role: worker
+- role: worker
+EOF
