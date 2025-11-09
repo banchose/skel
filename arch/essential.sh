fa9246e8dafe3a37b34a8e4d73f8646d8f226eec
banchose <a_gitmail@ownmail.net>
2025-11-09 15:21:27 -0500

    who

--- arch/essential.sh
+++ arch/essential.sh
@@ -179,6 +179,12 @@ sudo pacman -S arch-wiki-docs --needed --noconfirm
 
 sudo pacman -S devtools --needed --noconfirm
 
+# latex
+sudo pacman -S texlive-core texlive-bin --needed --noconfirm
+sudo pacman -S texlive-fontsrecommended --needed --noconfirm
+sudo pacman -S texlive-latexextra --needed --noconfirm
+sudo pacman -S texlive-binextra --needed --noconfirm
+
 # Experimental
 sudo pacman -S tidy --needed --noconfirm
 sudo pacman -S httrack --needed --noconfirm
@@ -242,6 +248,136 @@ yay -S jc jo yq htmlq jless xsv gron ripgrep-all moreutils entr --needed
 sudo pacman -S words --needed --noconfirm
 sudo pacman -S hunspell --needed --noconfirm
 sudo pacman -S hunspell-en_US --needed --noconfirm
+
+# pipx
+#
+# set -xuo pipefail
+
+# export PATH="$HOME/.local/bin:$PATH"
+# export PATH="$HOME/go/bin:$PATH"
+
+# For Alpine Linux
+if grep 'ID=alpine' /etc/os-release; then
+  apk update
+  apk add python3-dev
+  apk add py3-pip
+  apk add pipx
+  apk add rust
+  apk add cargo
+  apk add git
+  apk add make gcc musl-dev
+  apk add musl-dev
+  apk add make
+  apk add cmake
+  apk add nmap
+  apk add ninja
+  apk add gcc
+  apk add gettext-dev
+  apk add curl
+  apk add tmux
+  apk add jq
+  apk add gawk
+  apk add sed
+  apk add grep
+  apk add iproute2
+fi
+
+command -v pipx || {
+  echo "no pipx"
+  exit 1
+}
+
+pipx install checkov --include-deps
+pipx install ipython --include-deps
+pipx install flask --include-deps
+pipx install django --include-deps
+pipx install pandas --include-deps
+pipx install speedtest --include-deps
+pipx install httpie --include-deps
+pipx install pgcli
+# pipx install mycli
+pipx install pycowsay
+pipx install esptool
+pipx install litecli
+pipx install ruff
+pipx install black
+pipx install flake8
+pipx install pylint
+pipx install cfn-lint
+# pipx install posting
+pipx install rust
+pipx install pipenv
+pipx install poetry
+pipx install glances
+pipx install isort
+# pipx install jupyter
+pipx install llm
+pipx install aider-chat
+# pipx install ansible
+pipx install cfn-lsp-extra
+pipx install cookiecutter
+pipx install csvkit
+pipx install elia-chat
+# pipx install openai
+pipx install ptpython
+pipx install s3cmd
+pipx install shell-functools
+pipx install speedtest-cli
+pipx install twisted
+pipx install visidata
+pipx install psutils
+pipx install uv
+
+pipx inject llm llm-tools-exa --pip-args="--upgrade"
+pipx inject llm llm-openrouter --pip-args="--upgrade"
+pipx inject llm llm-anthropic --pip-args="--upgrade"
+pipx inject llm llm-fragments-github --pip-args="--upgrade"
+pipx inject llm llm-tools-simpleeval --pip-args="--upgrade"
+pipx inject llm llm-cmd-comp --pip-args="--upgrade"
+pipx inject llm llm-cmd --pip-args="--upgrade"
+pipx inject llm llm-python --pip-args="--upgrade" --force
+pipx inject llm llm-jq --pip-args="--upgrade" --force
+pipx inject llm llm-templates-fabric --pip-args="--upgrade"
+pipx inject llm howdoi --pip-args="--upgrade"
+pipx inject llm httpx --pip-args="--upgrade"
+pipx inject llm psutils --pip-args="--upgrade"
+pipx inject llm beautifulsoup4 --pip-args="--upgrade"
+pipx inject llm certifi --pip-args="--upgrade"
+pipx inject llm uv --pip-args="--upgrade"
+
+pipx inject \
+  ipython \
+  boto3 \
+  boltons \
+  beautifulsoup4 \
+  psutils \
+  howdoi \
+  pydantic \
+  cryptography \
+  black \
+  pendulum \
+  icecream \
+  loguru \
+  certifi \
+  pydantic \
+  httpx \
+  pandas \
+  esptool \
+  numpy \
+  ipython-extensions \
+  llm \
+  llm-tools-exa \
+  llm-openrouter \
+  llm-fragments-github \
+  llm-anthropic \
+  llm-tools-simpleeval \
+  llm-python \
+  llm-jq \
+  httpie \
+  uv \
+  --pip-args="--upgrade"
+
+pipx ensurepath
 
 #### ###LEGACY#### Wireless ####LEGACY#
 #### sudo pacman -S dialog --needed --noconfirm
