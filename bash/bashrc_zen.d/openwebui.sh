477d03ad6a1a3b9c815165cb4be5f30259bf74d6
banchose <a_gitmail@ownmail.net>
2025-12-04 08:42:05 -0500

    who

--- bash/bashrc_zen.d/openwebui.sh
+++ bash/bashrc_zen.d/openwebui.sh
@@ -1,1 +1,3 @@
 alias killopenwebui='fuser -k 8080/tcp;fuser -k 4000/tcp;fuser -k 8000/tcp;fuser -k 8001/tcp;fuser -k 8002/tcp;fuser -k 8003/tcp'
+
+alias getrag="kubectl logs -f --context arn:aws:eks:us-east-1:405350004483:cluster/EksTest statefulset/open-webui -n openwebui | grep vector"
