12855c35c708bbccc3de30a4c2a20c60e62bf918
banchose <a_gitmail@ownmail.net>
2025-12-11 15:40:37 -0500

    who

--- docker/builds/py0/testme.sh
+++ docker/builds/py0/testme.sh
@@ -14,6 +14,14 @@ aws --version
 cmake --version
 gcc --version
 make --version
+rustc --version
+dig -v
+jq -v
+awk
+curl | grep -q gnu
+wget --version | grep -q GNU
+ip -br a
+
 ping -c 3 1.1.1.1
 
 aws sso login --no-browser --use-device-code
