bf84aca9e4f93c413fd765bc4218abd68217fc44
banchose <a_gitmail@ownmail.net>
2025-12-12 16:46:46 -0500

    WORKS py1

--- bash/bashrc_zen.d/docker.sh
+++ bash/bashrc_zen.d/docker.sh
@@ -21,9 +21,15 @@ if type docker &>/dev/null; then
   alias trixie0='docker run -it --rm --name trixie0 --hostname trixie0 -v ~/temp:/home/loon/temp trixie0:latest'
   alias trixie0r='docker run -u root -it --rm --name trixie0 --hostname trixie0 -v ~/temp:/home/loon/temp trixie0:latest'
   alias trixie0bld='docker build -t trixie0:latest --build-arg USER_UID="$(id -u)" --build-arg USER_GID="$(id -g)" .'
+
+  # py0
   alias py0='docker run -it --rm --name py0 --hostname py0 -v ~/temp:/home/loon/temp py0:latest'
   alias py0r='docker run --name py0 -u root -it --rm --hostname py0 -v ~/temp:/home/loon/temp py0:latest'
   alias py0b='cd ~/gitdir/skel/docker/builds/py0 && docker build -t py0:latest --build-arg USER_UID="$(id -u)" --build-arg USER_GID="$(id -g)" .'
+
+  # py1
+  alias py1='docker run -it --rm --name py1 --hostname py1 -v ~/temp:/home/loon/temp py1:latest' alias py1r='docker run --name py1 -u root -it --rm --hostname py1 -v ~/temp:/home/loon/temp py1:latest'
+  alias py1b='cd ~/gitdir/skel/docker/builds/py1 && docker build -t py1:latest --build-arg USER_UID="$(id -u)" --build-arg USER_GID="$(id -g)" .'
 
   function dconnet() {
 
