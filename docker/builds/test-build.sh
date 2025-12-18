c84f70c6173839928879117cd74c76b19b0682a0
banchose <a_gitmail@ownmail.net>
2025-12-18 19:47:17 +0000

    who

--- docker/builds/test-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -uo pipefail
-
-for i in ripgrep bat coreutils diffutils aichat findutils util-linux platform-info procps sed gping procs bottom broot hexyl xh fd rigrep bat eza dust zoxide zellij cargo-update; do
-  echo "Attempting to install: ${i}"
-  ./build-rust.sh "${i}"
-done
