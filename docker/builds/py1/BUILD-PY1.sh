13e208ab312bd2f68966c19baab5bed29a8d4567
banchose <a_gitmail@ownmail.net>
2025-12-26 18:15:33 -0500

    who

--- docker/builds/py1/BUILD-PY1.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-CONTAINERNAME="py1"
-
-cd ~/gitdir/skel/docker/builds/"${CONTAINERNAME}" &&
-  docker build --build-arg USER_UID="$(id -u)" --build-arg USER_GID="$(id -g)" -t "${CONTAINERNAME}":latest . &&
-  docker run -it --rm --name "${CONTAINERNAME}" --hostname "${CONTAINERNAME}" -v ~/temp:/home/loon/temp "${CONTAINERNAME}"
--- docker/builds/py1/scripts/init-lazy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -xeuo pipefail
-
-mkdir -p -- "$HOME/.config/nvim"
-cp -r -- "$HOME/gitdir/starter/." "$HOME/.config/nvim"
