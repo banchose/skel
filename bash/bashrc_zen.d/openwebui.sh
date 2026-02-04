2594d7f0192ba3ef9224f912c96acbc3139790bd
banchose <a_gitmail@ownmail.net>
2026-02-04 16:35:21 -0500

    gitseal

--- bash/bashrc_zen.d/openwebui.sh
+++ bash/bashrc_zen.d/openwebui.sh
@@ -3,3 +3,5 @@ alias killopenwebui='fuser -k 8080/tcp;fuser -k 4000/tcp;fuser -k 8000/tcp;fuser
 alias getrag="kubectl logs -f --context arn:aws:eks:us-east-1:405350004483:cluster/EksTest statefulset/open-webui -n openwebui | grep vector"
 
 alias getwebui="aws s3 cp s3://openwebui-db-backups/webui.db ~/Downloads/webui.db-"$(date +%s)" --region us-east-1 --profile test"
+
+alias get_webui_logs='kubectl logs --context arn:aws:eks:us-east-1:405350004483:cluster/EksTest -n openwebui statefulsets/open-webui  -f'
