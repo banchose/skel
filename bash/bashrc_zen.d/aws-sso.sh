1c9346bc5bb6f38903582eac481fda5fd1382187
banchose <a_gitmail@ownmail.net>
2026-02-20 14:59:38 -0500

    gitseal

--- bash/bashrc_zen.d/aws-sso.sh
+++ bash/bashrc_zen.d/aws-sso.sh
@@ -1,20 +1,22 @@
-SSO_INSTANCE_ARN="arn:aws:sso:::instance/ssoins-722356dbd9c212b8"
+awlbedrockaccess() {
+  local sso_instance_arn="arn:aws:sso:::instance/ssoins-722356dbd9c212b8"
 
-# Loop through each permission set to find BedrockDeveloperAccess
-for PS_ARN in $(aws sso-admin list-permission-sets \
-  --instance-arn $SSO_INSTANCE_ARN \
-  --profile man \
-  --query 'PermissionSets[]' \
-  --output text); do
-
-  NAME=$(aws sso-admin describe-permission-set \
-    --instance-arn $SSO_INSTANCE_ARN \
-    --permission-set-arn $PS_ARN \
+  # Loop through each permission set to find BedrockDeveloperAccess
+  for PS_ARN in $(aws sso-admin list-permission-sets \
+    --instance-arn $sso_instance_arn \
     --profile man \
-    --query 'PermissionSet.Name' \
-    --output text)
+    --query 'PermissionSets[]' \
+    --output text); do
 
-  echo "Permission Set: $NAME"
-  echo "  ARN: $PS_ARN"
-  echo ""
-done
+    NAME=$(aws sso-admin describe-permission-set \
+      --instance-arn $sso_instance_arn \
+      --permission-set-arn $PS_ARN \
+      --profile man \
+      --query 'PermissionSet.Name' \
+      --output text)
+
+    echo "Permission Set: $NAME"
+    echo "  ARN: $PS_ARN"
+    echo ""
+  done
+}
