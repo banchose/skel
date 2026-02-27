528e598229331513646411ef32ef02b0ebaa036a
banchose <a_gitmail@ownmail.net>
2026-02-27 17:20:52 -0500

    gitseal

--- init/aws_yum_init.sh
+++ init/aws_yum_init.sh
@@ -2,37 +2,37 @@
 
 set -xeuo pipefail
 
-localectl set-locale LANG=en_US.UTF-8
-timedatectl set-timezone America/New_York
-hostnamectl set-hostname "aws-prd-tomcat-1"
-yum install -y wireshark-cli
-yum install -y tmux
-yum install -y mtr
-yum install -y jq
-yum install -y cowsay
-yum install -y git
-yum install -y python3
-yum install -y python3-pip
-yum install -y golang
-yum install -y cargo
-yum install -y gettext
-yum install -y ninja-build
-yum install -y cmake
-yum install -y gcc
-yum install -y unzip
-yum install -y gettext
-yum install -y make
-yum install -y nmap
-yum install -y unzip --allowerasing
-yum install -y gettext
-yum install -y curl --allowerasing
-yum install -y cargo
-yum install -y rust
-yum install -y rust-doc
-yum install -y nmap
-yum install -y p7zip
-yum install -y p7zip-plugins
-yum install -y p7zip-doc
-yum install -y socat
-yum install -y ncurses
-yum install -y golang
+sudo localectl set-locale LANG=en_US.UTF-8
+sudo timedatectl set-timezone America/New_York
+sudo hostnamectl set-hostname "aws-prd-tomcat-1"
+sudo yum install -y wireshark-cli
+sudo yum install -y tmux
+sudo yum install -y mtr
+sudo yum install -y jq
+sudo yum install -y cowsay
+sudo yum install -y git
+sudo yum install -y python3
+sudo yum install -y python3-pip
+sudo yum install -y golang
+sudo yum install -y cargo
+sudo yum install -y gettext
+sudo yum install -y ninja-build
+sudo yum install -y cmake
+sudo yum install -y gcc
+sudo yum install -y unzip
+sudo yum install -y gettext
+sudo yum install -y make
+sudo yum install -y nmap
+sudo yum install -y unzip --allowerasing
+sudo yum install -y gettext
+sudo yum install -y curl --allowerasing
+sudo yum install -y cargo
+sudo yum install -y rust
+sudo yum install -y rust-doc
+sudo yum install -y nmap
+sudo yum install -y p7zip
+sudo yum install -y p7zip-plugins
+sudo yum install -y p7zip-doc
+sudo yum install -y socat
+sudo yum install -y ncurses
+sudo yum install -y golang
