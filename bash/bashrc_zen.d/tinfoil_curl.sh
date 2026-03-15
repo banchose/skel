e704ce445a3f913965728ca73921cf4fe2c40dda
banchose <a_gitmail@ownmail.net>
2026-03-15 00:13:08 -0400

    gitseal

---
+++ bash/bashrc_zen.d/tinfoil_curl.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+## Tinfoil Models
+# deepseek-r1-0528
+# kimi-k2-5
+# gpt-oss-120b
+# llama3-3-70b
+
+tinllm() {
+  curl -sS https://inference.tinfoil.sh/v1/chat/completions -H "Authorization: Bearer ${TINFOIL_API_KEY}" \
+    -H "Content-Type: application/json" \
+    -d '{
+  "model": "kimi-k2-5",
+  "messages": [{"role": "user", "content": "Hello, world!"}],
+  "temperature": 0
+}'
+}
