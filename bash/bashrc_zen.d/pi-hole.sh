55a43e13a8c7d10f9789f6c8467952c414b8fe67
banchose <a_gitmail@ownmail.net>
2026-03-20 22:35:17 -0400

    gitseal

--- bash/bashrc_zen.d/pi-hole.sh
+++ bash/bashrc_zen.d/pi-hole.sh
@@ -4,7 +4,8 @@ pihole_get_auth() {
     echo "PIHOLE_ADMIN_PASS is not set"
     return 1
   }
-  curl -k -X POST "https://pi.hole/api/auth" \
+  #  curl -s -k -X POST "https://pi.hole/api/auth" \
+  curl -s -X POST "https://ns2.xaax.dev/api/auth" \
     --data '{"password":"'"${PIHOLE_ADMIN_PASS}"'"}'
 }
 
@@ -14,5 +15,5 @@ pihole_get() {
     return 1
   }
 
-  curl -k "https://pi.hole/api/stats/summary?sid=${PIHOLE_SID}"
+  curl -s "https://ns2.xaax.dev/api/stats/summary?sid=${PIHOLE_SID}"
 }
