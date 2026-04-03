b4e157e98db5f984cd47ae22deb2b3a4ad38c435
banchose <a_gitmail@ownmail.net>
2026-04-03 13:13:53 -0400

    gitseal

--- bash/bashrc_zen.d/hri-pisal-deploy-helper.sh
+++ bash/bashrc_zen.d/hri-pisal-deploy-helper.sh
@@ -13,6 +13,20 @@ deploy-pisal-onprem() {
   kubectl apply --context Prod-Admin@Prod -f ./pisal-deploy-newprod.yaml
 }
 
+deploy-pisal-awsweb() {
+  cd ~/hrigit/Kubernetes/pisal/awswebprod/ || {
+    printf 'Cannot cd to dir\n' >&2
+    return 1
+  }
+  git pull || return 1
+  grep 'image:' ./pisal-deploy-awswebprod.yaml
+  read -r -p "Apply? (y/N): "
+  [[ ${REPLY} =~ ^[Yy]$ ]] || {
+    printf 'Cancelled.\n'
+    return 0
+  }
+  kubectl apply --context Prod-Admin@Prod -f ./pisal-deploy-awswebprod.yaml
+}
 deploy-pisalapi-awsweb() {
   cd ~/hrigit/Kubernetes/pisalapi/awswebprod/ || {
     printf 'Cannot cd to dir\n' >&2
