2edec80eb0aeb25b8b8a49a2018b8957f548a666
banchose <a_gitmail@ownmail.net>
2026-04-04 21:07:03 -0400

    gitseal

--- docker/builds/py0/UPDATE_CONFIGS.sh
+++ docker/builds/py0/UPDATE_CONFIGS.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 
 command cp -v -- ~/.bashrc ~/gitdir/skel/docker/builds/py0/bashrc
 command cp -v -- ~/gitdir/skel/llm/litellm/litellm.conf ~/gitdir/skel/docker/builds/py0/LLM
+command cp -v -- ~/gitdir/skel/llm/litellm/extra-openai-models.yaml ~/gitdir/skel/docker/builds/py0/LLM
+command rm -v -- ~/gitdir/skel/docker/builds/py0/LLM/templates/*.yaml
 command cp -v -- ~/gitdir/skel/llm/TEMPLATES/*.yaml ~/gitdir/skel/docker/builds/py0/LLM/templates
+command sed -i 's/una/loon/' ~/gitdir/skel/docker/builds/py0/LLM/templates/*.yaml
