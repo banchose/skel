58eed8dd244ab989da489d2c56bc5aed34053200
banchose <a_gitmail@ownmail.net>
2026-05-27 15:18:43 -0400

    gitseal

--- bash/bashrc_zen.d/hri-attmove-deploy-helper.sh
+++ bash/bashrc_zen.d/hri-attmove-deploy-helper.sh
@@ -1,59 +1,28 @@
-deploy-pisal-onprem() {
-  cd ~/hrigit/Kubernetes/pisal/newprod/ || {
+deploy-attmove-awsweb() {
+  cd ~/hrigit/Kubernetes/attmove/awswebprod/ || {
     printf 'Cannot cd to dir\n' >&2
     return 1
   }
   git pull || return 1
-  grep 'image:' ./pisal-deploy-newprod.yaml
+  grep 'image:' ./attmove-deploy-awswebprod.yaml
   read -r -p "Apply? (y/N): "
   [[ ${REPLY} =~ ^[Yy]$ ]] || {
     printf 'Cancelled.\n'
     return 0
   }
-  kubectl apply --context Prod-Admin@Prod -f ./pisal-deploy-newprod.yaml
+  kubectl apply --context arn:aws:eks:us-east-1:315633532929:cluster/EksProd -f ./attmove-deploy-awswebprod.yaml
 }
-
-deploy-pisal-awsweb() {
-  cd ~/hrigit/Kubernetes/pisal/awswebprod || {
+deploy-attmoveapi-awsweb() {
+  cd ~/hrigit/Kubernetes/attmoveapi/awswebprod/ || {
     printf 'Cannot cd to dir\n' >&2
     return 1
   }
   git pull || return 1
-  grep 'image:' ./pisal-deploy-awswebprod.yaml
+  grep 'image:' ./attmoveapi-deploy-awswebprod.yaml
   read -r -p "Apply? (y/N): "
   [[ ${REPLY} =~ ^[Yy]$ ]] || {
     printf 'Cancelled.\n'
     return 0
   }
-  kubectl apply --context arn:aws:eks:us-east-1:315633532929:cluster/EksProd -f ./pisal-deploy-awswebprod.yaml
-}
-
-deploy-pisalapi-awsweb() {
-  cd ~/hrigit/Kubernetes/pisalapi/awswebprod/ || {
-    printf 'Cannot cd to dir\n' >&2
-    return 1
-  }
-  git pull || return 1
-  grep 'image:' ./pisalapi-deploy-awswebprod.yaml
-  read -r -p "Apply? (y/N): "
-  [[ ${REPLY} =~ ^[Yy]$ ]] || {
-    printf 'Cancelled.\n'
-    return 0
-  }
-  kubectl apply --context arn:aws:eks:us-east-1:315633532929:cluster/EksProd -f ./pisalapi-deploy-awswebprod.yaml
-}
-
-deploy-pisalapi-onprem() {
-  cd ~/hrigit/Kubernetes/pisalapi/newprod/ || {
-    printf 'Cannot cd to dir\n' >&2
-    return 1
-  }
-  git pull || return 1
-  grep 'image:' ./pisalapi-deploy-newprod.yaml
-  read -r -p "Apply? (y/N): "
-  [[ ${REPLY} =~ ^[Yy]$ ]] || {
-    printf 'Cancelled.\n'
-    return 0
-  }
-  kubectl apply --context Prod-Admin@Prod -f ./pisalapi-deploy-newprod.yaml
+  kubectl apply --context arn:aws:eks:us-east-1:315633532929:cluster/EksProd -f ./attmoveapi-deploy-awswebprod.yaml
 }
