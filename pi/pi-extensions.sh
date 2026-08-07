7ef6231f60711e0d87ead9be03b764a344d1af44
banchose <a_gitmail@ownmail.net>
2026-08-07 18:28:43 -0400

    gitseal

---
+++ pi/pi-extensions.sh
@@ -0,0 +1,2 @@
+pi install git:github.com/nagisanzenin/engram
+pi install npm:pi-mcp-adapter
--- pi/run-pi-bed.sh
+++ pi/run-pi-bed.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 cd /tmp
 docker run --rm -it \
   -e AWS_BEARER_TOKEN_BEDROCK \
+  -e EXA_API_KEY \
   -v "$PWD:/workspace" \
   -v pi-agent-home:/home/node/.pi/agent \
   pi-sandbox
