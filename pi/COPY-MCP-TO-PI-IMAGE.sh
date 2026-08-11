9c2e4f1119744fab81c2b27a99311eadc97da0ee
banchose <a_gitmail@ownmail.net>
2026-08-11 16:56:49 -0400

    gitseal

--- pi/COPY-MCP-TO-PI-IMAGE.sh
+++ pi/COPY-MCP-TO-PI-IMAGE.sh
@@ -14,5 +14,6 @@ docker run --rm \
       pi install npm:pi-mcp-adapter
       pi install npm:@narumitw/pi-plan-mode
       pi install npm:@narumitw/pi-btw
-      pi install npm:@narumitw/pi-stamp'
+      pi install npm:@narumitw/pi-stamp
+      pi install npm:@ff-labs/pi-fff'
 # pi install npm:@gotgenes/pi-permission-system
--- pi/pi-extensions.sh
+++ pi/pi-extensions.sh
@@ -1,4 +1,8 @@
 pi install git:github.com/nagisanzenin/engram
 pi install npm:@dietrichgebert/ponytail
 pi install npm:pi-mcp-adapter
+pi install npm:@narumitw/pi-plan-mode
+pi install npm:@narumitw/pi-btw
+pi install npm:@narumitw/pi-stamp
+pi install npm:@ff-labs/pi-fff
 # pi install npm:@gotgenes/pi-permission-system
