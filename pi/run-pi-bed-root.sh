013fdd4e0eb74887f3a1714d3313e216b94877e3
banchose <a_gitmail@ownmail.net>
2026-08-11 10:45:44 -0400

    gitseal

---
+++ pi/run-pi-bed-root.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# if [[ -d ~/temp ]]; then
+#   cd ~/temp
+# else
+#   echo "~/temp does not exist"
+# fi
+
+ENTRYPOINT_ARGS=()
+if [[ "${1:-}" == "-b" ]]; then
+  ENTRYPOINT_ARGS=(--entrypoint bash)
+fi
+
+docker run --rm -it \
+  "${ENTRYPOINT_ARGS[@]}" \
+  -e AWS_BEARER_TOKEN_BEDROCK \
+  -e EXA_API_KEY \
+  -e LAT \
+  -e LON \
+  -e OPENWEATHER_APP_ID \
+  -v "$PWD:/workspace" \
+  -v pi-agent-home:/home/node/.pi/agent \
+  -u 0:0 \
+  pi-sandbox
