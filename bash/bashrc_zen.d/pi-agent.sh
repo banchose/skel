524fd746ec27c2cdb463dc6f05d2b3dd359f2b88
banchose <a_gitmail@ownmail.net>
2026-08-31 18:59:14 -0400

    hwo

--- bash/bashrc_zen.d/pi-agent.sh
+++ bash/bashrc_zen.d/pi-agent.sh
@@ -12,5 +12,5 @@ pi-bed() {
     entrypoint_args=(-u 0 --entrypoint bash)
     shift
   fi
-  docker run --rm -it "${entrypoint_args[@]}" -e ANTHROPIC_API_KEY -e AWS_BEARER_TOKEN_BEDROCK -e EXA_API_KEY -e LAT -e LON -e OPENWEATHER_APP_ID -e SHELL=/bin/bash -e TINFOIL_API_KEY -v "$PWD:/workspace" -v pi-agent-home:/home/node/.pi/agent pi-sandbox "$@"
+  docker run --rm -it "${entrypoint_args[@]}" -e EDITOR="nvim" -e ANTHROPIC_API_KEY -e AWS_BEARER_TOKEN_BEDROCK -e EXA_API_KEY -e LAT -e LON -e OPENWEATHER_APP_ID -e SHELL=/bin/bash -e TINFOIL_API_KEY -v "$PWD:/workspace" -v pi-agent-home:/home/node/.pi/agent pi-sandbox "$@"
 }
