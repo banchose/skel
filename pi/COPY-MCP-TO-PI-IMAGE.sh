4d4ee02540212b2815b3b24b10bad53624512f4c
banchose <a_gitmail@ownmail.net>
2026-09-01 17:37:15 -0400

    gitseal

--- pi/COPY-MCP-TO-PI-IMAGE.sh
+++ pi/COPY-MCP-TO-PI-IMAGE.sh
@@ -28,9 +28,10 @@ docker run --rm \
       pi install npm:@ff-labs/pi-fff
       pi install npm:@juicesharp/rpiv-ask-user-question
       pi install npm:@juicesharp/rpiv-todo
-      pi install npm:awesome-pi-themes
       pi install npm:@firstpick/pi-themes-bundle
       pi update --extensions
       pi update --models'
+
+#      pi install npm:awesome-pi-themes
 # pi install npm:@gotgenes/pi-permission-system
 # npm ci
